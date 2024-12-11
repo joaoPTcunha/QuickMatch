cd8d7dd1640e07a3d39e12e07a0ab351bb447949
JoaoCunha <jpedro.seara@gmail.com>
2024-12-11 19:05:33 +0000

    admin agora feito e pequenas melhorias

--- resources/views/admin/user-management.blade.php
+++ resources/views/admin/user-management.blade.php
@@ -93,15 +93,16 @@
                                             <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                         </svg>
                                     </a>
-                                    <form action="{{ route('users.destroy', $user->id) }}" method="POST">
+                                    <form id="delete-user-form-{{ $user->id }}" action="{{ route('users.destroy', $user->id) }}" method="POST">
                                         @csrf
                                         @method('DELETE')
-                                        <button type="submit" class="text-red-500 hover:text-red-600">
+                                        <button type="button" onclick="confirmDeleteUser({{ $user->id }})" class="text-red-500 hover:text-red-600">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                             </svg>
                                         </button>
                                     </form>
+
                                 </div>
                             </td>
                         </tr>
@@ -112,8 +113,7 @@
 
             <!-- modal ver -->
             @foreach($users as $user)
-            <div id="modal-{{ $user->id }}"
-                class="fixed inset-0 z-50 bg-gray-800 bg-opacity-50 opacity-0 pointer-events-none flex items-center justify-center transition-opacity duration-300">
+            <div id="modal-{{ $user->id }}" class="fixed inset-0 z-50 bg-gray-800 bg-opacity-50 opacity-0 pointer-events-none flex items-center justify-center transition-opacity duration-300 px-2">
                 <div class="bg-white p-8 rounded-lg shadow-xl max-w-md w-full relative">
                     <button onclick="closeModal('modal-{{ $user->id }}')"
                         class="absolute top-4 right-4 text-gray-600 hover:text-gray-800 focus:outline-none">
@@ -153,9 +153,11 @@
             </div>
 
             <!-- modal editar -->
-            <div id="edit-modal-{{ $user->id }}" class="fixed inset-0 z-50 bg-gray-800 bg-opacity-50 flex items-center justify-center transition-opacity duration-300 opacity-0 pointer-events-none">
-                <div class="bg-white p-8 rounded-lg shadow-xl max-w-4xl w-full relative">
-                    <button onclick="closeModal('edit-modal-{{ $user->id }}')" class="absolute top-4 right-4 text-gray-600 hover:text-gray-800 focus:outline-none">
+            <div id="edit-modal-{{ $user->id }}"
+                class="fixed inset-0 z-50 bg-gray-800 bg-opacity-50 flex items-center justify-center transition-opacity duration-300 opacity-0 pointer-events-none px-3 mt-3">
+                <div class="bg-white p-8 rounded-lg shadow-xl max-w-4xl w-full relative overflow-auto max-h-screen sm:px-6">
+                    <button onclick="closeModal('edit-modal-{{ $user->id }}')"
+                        class="absolute top-4 right-4 text-gray-600 hover:text-gray-800 focus:outline-none">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
@@ -251,11 +253,6 @@
                             </button>
                         </div>
                     </form>
-
-
-
-
-
                 </div>
             </div>
 
@@ -298,6 +295,25 @@
             output.src = reader.result;
         };
         reader.readAsDataURL(event.target.files[0]);
+    }
+
+    function confirmDeleteUser(userId) {
+        const form = document.getElementById(`delete-user-form-${userId}`);
+
+        Swal.fire({
+            title: 'Tem a certeza que deseja apagar este utilizador?',
+            text: "Esta ação não pode ser desfeita.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Sim, apagar!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                form.submit();
+            }
+        });
     }
 
     document.getElementById('dropdownButton').addEventListener('click', function(e) {
